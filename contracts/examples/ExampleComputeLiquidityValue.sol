d0da90b33305b95727322502b5f07afaaa6893e5
Absarsdq291 <absarsdq291@gmail.com>
2024-08-15 18:08:46 +0500

    initial commit

---
+++ contracts/examples/ExampleComputeLiquidityValue.sol
@@ -0,0 +1,90 @@
+pragma solidity =0.6.6;
+
+import '../libraries/FiveswapV2LiquidityMathLibrary.sol';
+
+contract ExampleComputeLiquidityValue {
+    using SafeMath for uint256;
+
+    address public immutable factory;
+
+    constructor(address factory_) public {
+        factory = factory_;
+    }
+
+    // see FiveswapV2LiquidityMathLibrary#getReservesAfterArbitrage
+    function getReservesAfterArbitrage(
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB
+    ) external view returns (uint256 reserveA, uint256 reserveB) {
+        return FiveswapV2LiquidityMathLibrary.getReservesAfterArbitrage(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB
+        );
+    }
+
+    // see FiveswapV2LiquidityMathLibrary#getLiquidityValue
+    function getLiquidityValue(
+        address tokenA,
+        address tokenB,
+        uint256 liquidityAmount
+    ) external view returns (
+        uint256 tokenAAmount,
+        uint256 tokenBAmount
+    ) {
+        return FiveswapV2LiquidityMathLibrary.getLiquidityValue(
+            factory,
+            tokenA,
+            tokenB,
+            liquidityAmount
+        );
+    }
+
+    // see FiveswapV2LiquidityMathLibrary#getLiquidityValueAfterArbitrageToPrice
+    function getLiquidityValueAfterArbitrageToPrice(
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB,
+        uint256 liquidityAmount
+    ) external view returns (
+        uint256 tokenAAmount,
+        uint256 tokenBAmount
+    ) {
+        return FiveswapV2LiquidityMathLibrary.getLiquidityValueAfterArbitrageToPrice(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB,
+            liquidityAmount
+        );
+    }
+
+    // test function to measure the gas cost of the above function
+    function getGasCostOfGetLiquidityValueAfterArbitrageToPrice(
+        address tokenA,
+        address tokenB,
+        uint256 truePriceTokenA,
+        uint256 truePriceTokenB,
+        uint256 liquidityAmount
+    ) external view returns (
+        uint256
+    ) {
+        uint gasBefore = gasleft();
+        FiveswapV2LiquidityMathLibrary.getLiquidityValueAfterArbitrageToPrice(
+            factory,
+            tokenA,
+            tokenB,
+            truePriceTokenA,
+            truePriceTokenB,
+            liquidityAmount
+        );
+        uint gasAfter = gasleft();
+        return gasBefore - gasAfter;
+    }
+}
