85531fafb285ff2475f1e1f8d0b4a42f8532c4b7
Absarsdq291 <absarsdq291@gmail.com>
2025-01-24 16:09:36 +0500

    remove comment

--- contracts/FiveswapV2Router02.sol
+++ contracts/FiveswapV2Router02.sol
@@ -290,7 +290,7 @@ contract FiveswapV2Router02 is IFiveswapV2Router02 {
             token,
             to,
             IERC20(token).balanceOf(address(this))
-        ); //TBD about how to tackle this going forward
+        ); 
         IWPEN(WPEN).withdraw(amountPEN);
         TransferHelper.safeTransferPEN(to, amountPEN);
     }
