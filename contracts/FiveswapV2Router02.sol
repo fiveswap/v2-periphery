36310702c038c871dc5563bffde0f518b69405ce
syed mohib <syedMohib44>
2025-01-24 18:39:44 +0500

    Fixed issue with removeLiquidityPENSupportingFeeOnTransferTokens

--- contracts/FiveswapV2Router02.sol
+++ contracts/FiveswapV2Router02.sol
@@ -288,11 +288,15 @@ contract FiveswapV2Router02 is IFiveswapV2Router02 {
             address(this),
             deadline
         );
-        TransferHelper.safeTransfer(
-            token,
-            to,
-            IERC20(token).balanceOf(address(this))
-        ); 
+        uint finalBalance = IERC20(token).balanceOf(address(this));
+        uint receivedAmount = finalBalance > initialBalance
+            ? finalBalance - initialBalance
+            : 0;
+
+        // Ensure the received amount is greater than 0
+        require(receivedAmount > 0, "No tokens received");
+
+        TransferHelper.safeTransfer(token, to, receivedAmount);
         IWPEN(WPEN).withdraw(amountPEN);
         TransferHelper.safeTransferPEN(to, amountPEN);
     }
